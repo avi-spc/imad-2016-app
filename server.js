dec59095abc63eb90e5e438050d978c908cbcc96
avi-spc <avdhesh7394vn@gmail.com>
2016-11-08 10:49:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/ui/login.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'login.html'));
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
