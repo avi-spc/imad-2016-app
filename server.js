a6304842c8e7de23630b5ea66bff53a350175861
avi-spc <avdhesh7394vn@gmail.com>
2016-12-02 13:37:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,55 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
-
+var content={
+    query:'mysqli_query("insert into Login values($Username,$Password)")',
+    User:'$_POST["user"]',
+    Pass:'$_POST["pass"]',
+    co:`    <html>
+    
+    <head>
+        <title>Register</title>
+    </head>
+        <body>
+        <form action='reg.php' method='POST'>
+            Username:<input type='text' name='user' /><br>
+            Password:<input type='password' name='pass' /><br>
+            <input type='submit' name='sub' value='Submit' />
+        </form>
+        </body>
+</html>`
+};
+function createCon(data){
+    var query=data.query;
+    var User=data.User;
+    var Pass=data.Pass;
+var con=`<?php
+    if(isset($_POST['sub']))
+    {
+    $Username=${User}
+    $Password=${Pass}
+    
+    $conn=mysqli_connect("localhost:5432","avi-spc","db-avi-spc-65624","avi-spc");
+    if(${query})
+    {
+    echo"Valus stored successfully";
+		}
+    else{
+    echo"Failed";
+    }
+    
+    }
+    ?>
+`;
+    return con;
+}
+app.get('/ui/reg.html', function (req, res) {
+  res.send(createCon(content));
+    
+});
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
